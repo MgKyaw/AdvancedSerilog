c01b99ea6e5871fe922472a954aaec8c5d08d5a6
MgKyaw <mgkyaw20112@gmail.com>
2025-08-24 21:27:30 +0700

    Add project files.

---
+++ AdvancedSerilog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdvancedSerilog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
